d2f775f8313e94c6d6a7ce8b4a4ee571c1193676
Patrick <njorogepatrick739@gmail.com>
2024-07-25 12:56:06 +0300

    submit

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solutions in this file
+function writeCards(names, gift) {
+  
+  let thankYouMessages = [];
+
+  
+  names.forEach((name) => {
+    
+    thankYouMessages.push(
+      `Thank you, ${name}, for the wonderful ${gift} gift!`
+    );
+  });
+
+  
+  return thankYouMessages;
+}
+
+
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+function countDown(num) {
+  
+  for (let i = num; i >= 0; i--) {
+    
+    console.log(i);
+  }
+}
+
+
+countDown(10);
